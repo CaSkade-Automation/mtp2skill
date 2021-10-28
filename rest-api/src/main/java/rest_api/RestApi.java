17ced2ef29849cb2e8db4660d096905aadeeba06
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-10-28 16:32:49 +0200

    Minor cosmetic changes

--- rest-api/src/main/java/rest_api/RestApi.java
+++ rest-api/src/main/java/rest_api/RestApi.java
@@ -18,47 +18,38 @@ public class RestApi {
 
 	public static void main(String[] args) {
 
-		port(9191);								// Set port of this webservice
+		port(9191); // Set port of this webservice
 		System.out.println("Running MTP-Mapping-Service at localhost:9191");
-		
-		File uploadDir = new File("upload");	
-		uploadDir.mkdir();						// Create new folder if it doesn't exist and add it to static files folder
+
+		File uploadDir = new File("upload");
+		uploadDir.mkdir(); // Create new folder if it doesn't exist and add it to static files folder
 		staticFiles.externalLocation("upload");
-		
-		
+
 		// Setup a post route at base path
 		post("/", (request, response) -> {
 			request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
-			
+
 			Part uploadedFileObject = request.raw().getPart("mtp-file");
-			
+
 			File file = new File(uploadDir.getAbsolutePath() + File.separator + getFileName(uploadedFileObject));
 			file.deleteOnExit();
-			
 
 			// make sure to send the file as a multipart/form-data with key "mtp-file"
 			try (InputStream input = request.raw().getPart("mtp-file").getInputStream()) {
-				Files.copy(input, file.toPath() , StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			}
 
-			System.out.println("Uploaded file '" + getFileName(request.raw().getPart("mtp-file")) + "' saved as '"
-					+ file + "'");
-			
-//			Thread thread = new Thread() {
-//				public void run() {
-					String mappingResult = mapping.executeMapping(file.toString());
-//					System.out.println("Mapping done");
-//				}
-//			};
-//			thread.start();
+			System.out.println("Uploaded file '" + getFileName(request.raw().getPart("mtp-file")) + "' saved as '" + file + "'");
+
+			String mappingResult = mapping.executeMapping(file.toString());
 
 			return mappingResult;
 		});
 	}
 
-
 	/**
 	 * Returns the file name of one part (i.e. one file) of a multipart file-upload
+	 * 
 	 * @param part
 	 * @return
 	 */
